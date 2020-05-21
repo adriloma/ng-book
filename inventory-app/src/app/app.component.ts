54aadcc46c7e3eb3c4faf5210defcc1dfcc90514
Adrián López Martínez <adrian.lopez@ezzing.com>
2020-05-21 12:07:08 +0200

    chore(inventory-app): implemented inventory app

--- inventory-app/src/app/app.component.ts
+++ inventory-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-app';
+  products: Product[];
+  constructor() {
+    this.products = [];
+    this.products.push(new Product('NICEHAT', 'A nice black hay', '/assets/images/products/black-hat.jpg', ['men', 'accessories', 'hat'], 29.99));
+    this.products.push(new Product('MYSHOES', 'Black running shoes', '/assets/images/products/black-shoes.jpg', ['men', 'shoes', 'running shoes'], 109.99));
+    this.products.push(new Product('NEATOJACKET', 'Blue jacket', '/assets/images/products/blue-jacket.jpg', ['women', 'apparel', 'jackets & vests'], 238.99));
+  }
+
+  public productWasSelected(product: Product) {
+    console.log(`selected: ${product.name} with price ${product.price}`);
+  }
 }
